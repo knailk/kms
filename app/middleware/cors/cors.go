afa9a552191f6fbc2c50d6015b6b9685051746aa
knailk <tiendung3719@gmail.com>
2024-04-07 17:05:49 +0700

    fix: fix cors

--- app/middleware/cors/cors.go
+++ app/middleware/cors/cors.go
@@ -10,7 +10,7 @@ import (
 
 func CORS(cfg *config.Config) gin.HandlerFunc {
 	corsCfg := cors.Config{
-		AllowOrigins: cfg.HTTPServer.CORS.AllowOrigins,
+		AllowOrigins: []string{"http://localhost:3000"},
 		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders: []string{"Content-Length", "Origin", "cookie", "access-control-allow-origin",
 			"authorization, origin, content-type, accept", "X-CSRF-Token", "Pragma"},
