de2eae1374d0fc2f0f0e53bd9ad90ce424c119e2
Nguyen Danh Tien Dung <tiendung3719@gmail.com>
2024-04-10 17:20:03 +0700

    update: add field

--- cmd/migration/migrations/4_create_tables_chat_session.up.sql
+++ cmd/migration/migrations/4_create_tables_chat_session.up.sql
@@ -4,5 +4,7 @@ CREATE TABLE IF NOT EXISTS public.chat_sessions
     name text COLLATE pg_catalog."default",
     created_at timestamp with time zone,
     is_deleted bigint,
+    latest_message_id text COLLATE pg_catalog."default",
+    updated_at timestamp with time zone,
     CONSTRAINT chat_sessions_pkey PRIMARY KEY (id)
 );
