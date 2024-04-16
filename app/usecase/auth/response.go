6f86ac0b588f560a77e408253142d81a540c6756
knailk <tiendung3719@gmail.com>
2024-04-16 08:13:07 +0700

    fix: pointer

--- app/usecase/auth/response.go
+++ app/usecase/auth/response.go
@@ -15,8 +15,8 @@ type LoginResponse struct {
 	BirthDate    time.Time     `json:"birthDate"`
 	PictureURL   string        `json:"pictureURL"`
 	Address      string        `json:"address"`
-	Longitude    float64       `json:"longitude"`
-	Latitude     float64       `json:"latitude"`
+	Longitude    *float64      `json:"longitude"`
+	Latitude     *float64      `json:"latitude"`
 	CreatedAt    time.Time     `json:"createdAt"`
 	AccessToken  authjwt.Token `json:"-"`
 	RefreshToken authjwt.Token `json:"-"`
